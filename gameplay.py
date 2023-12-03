b6f6f7ba06c9c0a3e11a3ebaa9da25bc70d68739
JiWon Woo <jwoo26@jhu.edu>
2023-12-02 20:20:29 -0500

    added down movement, and clear line functions

--- gameplay.py
+++ gameplay.py
@@ -14,6 +14,7 @@ class Tetris:
         self.canvas = tk.Canvas(self.root, width=10*self._block_size, height=20*self._block_size)
         self.canvas.pack()
         self.tags = ['a','b','c','d']
+        self.pf_tags = []
         self.pf = Playfield()
         self.draw_pf()
         
@@ -25,16 +26,20 @@ class Tetris:
         self.root.bind("<Left>", self.left)
         self.root.bind("<Right>", self.right)
         self.root.bind("<Up>", self.rotate_t)
+        self.root.bind("<Down>", self.down)
         
 
     def draw_pf(self):
         '''creates playfield'''
-        for tag in self.tags:
+        for tag in self.pf_tags:
             self.canvas.delete(tag)
+            print(tag)
+        self.pf_tags = []
         for i in range(self.pf.pf.shape[0]):
             for j in range(self.pf.pf.shape[1]):
                 if self.pf.pf[i,j] != 0:
-                    self.canvas.create_rectangle(j * self._block_size, i * self._block_size, (j + 1) * self._block_size, (i + 1) * self._block_size, fill=self._color_dict[self.pf.pf[i,j]])
+                    self.pf_tags.append('pf'+str(len(self.pf_tags)))
+                    self.canvas.create_rectangle(j * self._block_size, i * self._block_size, (j + 1) * self._block_size, (i + 1) * self._block_size, fill=self._color_dict[self.pf.pf[i,j]], tags = self.pf_tags[-1])
 
     def draw_tetromino(self):
         idx = 0
@@ -49,22 +54,24 @@ class Tetris:
                                                  ((i+self.t.r)+ 1) * self._block_size, 
                                                  fill=self._color_dict[self.t.block[i,j]], tags=self.tags[idx])
                     idx +=1
+        self.check_line_clear()
 
     def gravity(self):
         ''
+        self.t.r += 1
         if self.check_edge() and self.check_hit(): #add or hits solid
             self.draw_tetromino()
             self.root.after(500, lambda: self.gravity())
-            self.t.r += 1
         else:
-            self.pf.update_pf(self.t)
+            self.t.r -= 1
+            self.pf.add_t(self.t)
             self.draw_pf()
             self.t = Tetromino(random.randint(1, 7))
             self.draw_tetromino()
             self.root.after(500, lambda: self.gravity())
     
     def check_edge(self):
-        if self.t.r + self.t.block.shape[0] >= 20:
+        if self.t.r + self.t.block.shape[0] >= 21:
             return False
         if (self.t.c < 0) or (self.t.c + self.t.block.shape[1]) > 10:
             return False
@@ -73,37 +80,49 @@ class Tetris:
     def check_hit(self):
         for i in range(self.t.block.shape[0]):
             for j in range(self.t.block.shape[1]):
-                if (self.t.block[i,j] != 0) and (self.pf.pf[i+self.t.r+1, j+self.t.c] != 0):
+                if (self.t.block[i,j] != 0) and (self.pf.pf[i+self.t.r, j+self.t.c] != 0):
                     return False
         return True
     
     def left(self, event):
         self.t.c -= 1
-        if self.check_edge():
+        if self.check_edge() and self.check_hit():
             self.draw_tetromino()
         else:
             self.t.c += 1
 
     def right(self, event):
         self.t.c += 1
-        if self.check_edge():
+        if self.check_edge() and self.check_hit():
             self.draw_tetromino()
         else:
             self.t.c -= 1
     
+    def down(self, event):
+        self.t.r += 1
+        if self.check_edge() and self.check_hit():
+            self.draw_tetromino()
+        else:
+            self.t.r -= 1
+    
     def rotate_t(self, event):
         self.t.rotate()
-        if self.check_edge():
-            if self.check_hit():
+        if self.check_edge() and self.check_hit():
                 self.draw_tetromino()
-            else:
-                self.pf.update_pf(self.t)
-                self.draw_pf()
-                self.t = Tetromino(random.randint(1, 7))
-                self.draw_tetromino()
-                self.root.after(500, lambda: self.gravity())
         else: 
             self.t.rotate(-1)
+    
+    def check_line_clear(self):
+        clear_line = []
+        for i, row in enumerate(self.pf.pf):
+            if np.count_nonzero(row) == 10:
+                clear_line.append(i)
+        for line in clear_line: 
+            print(line)           
+            self.pf.clear_line(line)
+            self.draw_pf()
+
+
 
 
 if __name__ == "__main__":
--- playfield.py
+++ playfield.py
@@ -5,8 +5,12 @@ class Playfield:
     def __init__(self):
         self.pf = np.zeros((20, 10))
     
-    def update_pf(self, t):
+    def add_t(self, t):
         for i in range(t.block.shape[0]):
             for j in range(t.block.shape[1]):
                 if t.block[i,j] != 0:
                     self.pf[i+t.r, j+t.c] = t.block[i,j]
+    
+    def clear_line(self, row):
+        self.pf = np.delete(self.pf, row, 0)
+        self.pf = np.insert(self.pf, 0, np.zeros(10), 0)
